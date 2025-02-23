75089650eed1ff5a8f1476f3be9683b4f190d476
shahabsaqib220 <shahabsaqib220@gmail.com>
2025-02-23 23:04:33 +0500

    Noice

--- server/index.js
+++ server/index.js
@@ -21,14 +21,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(cors({
-  origin: '*',
-  // Replace with your frontend's origin
+  origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
 }));
 
 // Use process.env.PORT for Vercel, fallback to 5000 for local development
-const PORT =5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 const { upload, gfs } = initGridFS();
@@ -48,6 +47,8 @@ app.use('/api/v1/groupmember/research', GroupMemberContribution);
 
 app.use(errorHandler);
 
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
